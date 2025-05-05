979dc558bcb9980252f80b5d460ad5ff56aa5b11
Chayesh002 <145770450+Chayesh002@users.noreply.github.com>
2025-05-05 15:53:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,16 +41,23 @@ const projects = [
         link: 'https://google.com'
     },
     {
-        title: 'Shock Tube - Surface Mining ',
+        title: ' Shock Tube Project – Blast Wave Simulation ',
         description: 'This project involved the use of a shock tube to replicate and study the effects of explosive blast waves in a controlled environment. Key responsibilities included preparing the experimental setup, triggering high-pressure shock waves, and analysing material responses under dynamic loading. The project highlighted expertise in explosive testing, safety compliance, and data analysis related to high-speed shock phenomena.',
         media: 'harmony-project2.mp4',
         type: 'video',
         link: '#'
     },
     {
-        title: 'Underground Mining POC',
+        title: 'Sandvik POC Project – VR Training SimulationC',
         description: 'This VR-based proof of concept was developed for Sandvik to simulate heavy machinery operations in a virtual mining environment. The interactive experience allowed users to engage with complex equipment procedures for training, safety demonstration, and operational planning. Built using Unity, the project showcased immersive 3D design, realistic physics, and intuitive user interactions—supporting Sandvik’s push toward innovative, tech-driven workforce development.',
         media: 'harmony-project3.mp4',
+        type: 'video',
+        link: '#'
+    },
+    {
+        title: ' WebGL Emulator Project',
+        description: 'This project is a Unity-based WebGL launcher designed to load multiple applications from a single interface. It uses a JSON configuration file to dynamically display buttons that launch individual WebGL builds. The system is lightweight, modular, and ideal for offline use, such as on MoodleBox devices. It demonstrates effective use of Unity UI, JSON parsing, and WebGL deployment strategies for scalable content delivery.',
+        media: 'harmony-project4.mp4',
         type: 'video',
         link: '#'
     }
